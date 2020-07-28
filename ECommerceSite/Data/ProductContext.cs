f6a32b83c53088832c682c187aee0517644ff3f2
Kevin Henneigh <55999394+kevinhenneigh@users.noreply.github.com>
2020-07-28 15:39:20 -0700

    Issue#4 create product db (#7)
    * Deleted default Bootstrap files
    
    * Added Bootstrap using LibMan
    
    * Updated bootstrap links in layout to use updated bootstrap files
    
    * Replaced jquery files with libman managed files
    
    * Added jquery validate with LibMan
    
    * Created Product model
    
    * Set Db Context
    
    * Added database connection string
    
    * Added EFCore Tools package
    
    * Added migration to create Db with products table

---
+++ ECommerceSite/Data/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿using ECommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceSite.Data
+{
+    public class ProductContext: DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options)
+            : base(options) {}
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
--- ECommerceSite/Startup.cs
+++ ECommerceSite/Startup.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ECommerceSite.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace ECommerceSite
 {
@@ -24,6 +27,9 @@ namespace ECommerceSite
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ProductContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
