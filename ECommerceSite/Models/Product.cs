e843815836e0abb93d555175d72ce953ff7c2a40
kevinhenneigh <kevinhenneigh@hotmail.com>
2020-07-28 12:56:06 -0700

    Created Product model

---
+++ ECommerceSite/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceSite.Models
+{
+    /// <summary>
+    /// A salable product
+    /// </summary>
+    public class Product
+    {
+        [Key] // make Primary Key in database
+        public int ProductId { get; set; } 
+
+        /// <summary>
+        /// The consumer facing name of the product
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The retsil price in US currency
+        /// </summary>
+        public double Price { get; set; }
+        
+        /// <summary>
+        /// Category product falls under
+        /// Ex. Electronics
+        /// </summary>
+        public string Category { get; set; }
+    }
+}
--- ECommerceSite/Startup.cs
+++ ECommerceSite/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace ECommerceSite
 {
@@ -24,6 +26,9 @@ namespace ECommerceSite
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ProductContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
