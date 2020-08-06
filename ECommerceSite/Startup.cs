27579924498c00f476059f1e8c9a6f3094d1b17f
Kevin Henneigh <55999394+kevinhenneigh@users.noreply.github.com>
2020-08-05 20:49:43 -0700

    Issue#6 product listing (#12)
    * cdeated product listing page
    
    * Found and fixed styling bugs
    
    * Refactored catalog to use model properties as column headers

--- ECommerceSite/Startup.cs
+++ ECommerceSite/Startup.cs
@@ -29,7 +29,7 @@ namespace ECommerceSite
             services.AddControllersWithViews();
 
             services.AddDbContext<ProductContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
