db2f89951096b621e5b54afbad4aaf4d8e689f09
kevinhenneigh <kevinhenneigh@hotmail.com>
2020-08-23 17:36:48 -0700

    Create register view model

--- ECommerceSite/Models/UserAccount.cs
+++ ECommerceSite/Models/UserAccount.cs
@@ -19,4 +19,26 @@ namespace ECommerceSite.Models
 
         public DateTime? DateOfBirth{ get; set; }
     }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Compare(nameof(Email))]
+        [Required]
+        public string ConfirmEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+    }
 }
