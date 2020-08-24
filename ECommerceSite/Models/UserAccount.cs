71c9260e4af9977de49cf286e5cb0e3fbdfb7b1e
Kevin Henneigh <55999394+kevinhenneigh@users.noreply.github.com>
2020-08-23 18:52:04 -0700

    Issue#22 registration (#25)
    * Creqted a user model
    
    * Added user account to database using code first migration
    
    * Create register view model
    
    * Added a register page and validation
    
    * Add registered user into Db

---
+++ ECommerceSite/Models/UserAccount.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceSite.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime? DateOfBirth{ get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(200)]
+        public string Email { get; set; }
+
+
+        [Compare(nameof(Email))]
+        [Required]
+        [Display(Name = "Confirm Email")]
+        public string ConfirmEmail { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(120, MinimumLength = 6, 
+            ErrorMessage = "Passweord must be between {2} and {1} characters")]
+        public string Password { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [StringLength(20)]
+        public string Username { get; set; }
+
+
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
