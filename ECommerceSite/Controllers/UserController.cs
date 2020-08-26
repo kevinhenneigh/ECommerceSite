073cf0718ab88b6b22c3e0172e7ce2d645b3a4e7
kevinhenneigh <kevinhenneigh@hotmail.com>
2020-08-25 20:10:49 -0700

    Login page error message added

--- ECommerceSite/Controllers/UserController.cs
+++ ECommerceSite/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using ECommerceSite.Data;
 using ECommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECommerceSite.Controllers
 {
@@ -48,6 +49,41 @@ namespace ECommerceSite.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            // Query syntax
+            //UserAccount account = await (from u in _context.UserAccounts
+            //                             where (u.Username == model.UsernameOrEmail
+            //                               || u.Email == model.UsernameOrEmail)
+            //                               && u.Password == model.Password
+            //                             select u).SingleOrDefaultAsync();
+            
+            //Same as above with method syntax
+            UserAccount account = await _context.UserAccounts.
+                Where(userAcc => (userAcc.Username == model.UsernameOrEmail ||
+                                   userAcc.Email == model.UsernameOrEmail) &&
+                                   userAcc.Password == model.Password)
+                .SingleOrDefaultAsync();
+            
+                if (account == null)
+            {
+                // Credentials did not match
+
+                // Custom error message
+                ModelState.AddModelError(string.Empty, "Credentials do not match our records");
+
+                return View(model);
+            }
+
+            // Log user into website
+            return RedirectToAction("Index", "Home");
         }
     }
 }
