9581933594aab41ee3c7209cb0f93e5ae805a98e
kevinhenneigh <kevinhenneigh@hotmail.com>
2020-08-27 09:02:31 -0700

    Single product can now be added to cart

--- ECommerceSite/Controllers/CartController.cs
+++ ECommerceSite/Controllers/CartController.cs
@@ -2,20 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ECommerceSite.Data;
+using ECommerceSite.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace ECommerceSite.Controllers
 {
     public class CartController : Controller
     {
-        public IActionResult Add(int id) // Id of the product to add
+        private readonly ProductContext _context;
+        private readonly IHttpContextAccessor _httpcontext;
+        public CartController(ProductContext context, IHttpContextAccessor httpContext)
         {
-            // Get product from the database
+            _context = context;
+            _httpcontext = httpContext;
+        }
 
-            // Add product to the cart
+        /// <summary>
+        /// Adds a product to the shopping cart
+        /// </summary>
+        /// <param name="id">The id of the product to add</param>
+        /// <returns></returns>
+        public async Task<IActionResult> Add(int id) // Id of the product to add
+        {
+            
+            Product product = await ProductDb.GetProductAsync(_context, id);
+
+            // Add product to the cart cookie
+            string data = JsonConvert.SerializeObject(product);
+            CookieOptions options = new CookieOptions()
+            {
+                Expires = DateTime.Now.AddYears(1),
+                Secure = true,
+                IsEssential = true
+            };
+
+            _httpcontext.HttpContext.Response.Cookies.Append("CartCookie", data, options);
 
             // Redirect to previous page
-            return View();
+            return RedirectToAction("Index", "Product");
         }
 
         public IActionResult Summary()
