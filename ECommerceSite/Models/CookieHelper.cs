1788e5291edbe2163b16d6195ef9a99e776fa456
kevinhenneigh <kevinhenneigh@hotmail.com>
2020-08-27 15:18:05 -0700

    Display item count in cart navigation

--- ECommerceSite/Models/CookieHelper.cs
+++ ECommerceSite/Models/CookieHelper.cs
@@ -52,7 +52,8 @@ namespace ECommerceSite.Models
 
         public static int GetTotalCartProducts(IHttpContextAccessor http)
         {
-            throw new NotImplementedException();
+            List<Product> cartProducts = GetCartProducts(http);
+            return cartProducts.Count;
         }
     }
 }
